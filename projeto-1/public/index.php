866413f2faa81e7401224c753adf28f53412cd2d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-05-16 15:56:20 -0300

    36. Iniciando FrontController

---
+++ projeto-1/public/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$url = substr($_SERVER['REQUEST_URI'], 1);
+$url = explode('/', $url);
+
+$controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
+
+print $controller;
